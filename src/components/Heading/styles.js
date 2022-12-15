17baa563b79e81f83b6b36fbddac47d41bc34227
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2022-12-15 10:52:56 -0300

    Update styles.js

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,39 +1,7 @@
 import styled, { css } from 'styled-components';
 
-const TitleSize = {
-  xsmall: (theme) => css`
-    font-size: ${theme.fonts.sizes.xsmall};
-  `,
-  small: (theme) => css`
-    font-size: ${theme.fonts.sizes.small};
-  `,
-  medium: (theme) => css`
-  font-size: ${theme.fonts.sizes.medium};
-  `,
-  large: (theme) => css`
-    font-size: ${theme.fonts.sizes.large};
-  `,
-  xlarge: (theme) => css`
-   font-size: ${theme.fonts.sizes.xlarge};
-  `,
-  xxlarge: (theme) => css`
-  font-size: ${theme.fonts.sizes.xxlarge};
-  `,
-  huge: (theme) => css`
-    font-size: ${theme.fonts.sizes.huge};
-  `,
-  xhuge: (theme) => css`
-    font-size: ${theme.fonts.sizes.xhuge};
-  `,
-};
-
-const titleCase = (uppercase) => css`
-  text-transform: ${uppercase ? 'uppercase' : 'none'};
-`;
-
 export const Title = styled.h1`
-  ${({ theme, colorDark, size, uppercase }) => css`
+  ${({ theme, colorDark, size }) => css`
     color: ${colorDark ? theme.colors.primaryColor : theme.colors.white};
-    ${TitleSize[size](theme)};
   `}
 `;
